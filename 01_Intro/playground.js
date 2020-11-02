4014bd01373e5b357b357beda74dc46375cabafb
Mario Lazzari <mario.lazzari@gmail.com>
2020-11-02 21:13:52 +0100

    Done chapter 1

---
+++ 01_Intro/playground.js
@@ -0,0 +1,21 @@
+// select db
+use("shop");
+
+// insert new item
+db.products.insertOne({ name: "A book", price: 9.95 });
+// insert new item
+db.products.insertOne({
+  name: "A new t-shirt",
+  price: 9.95,
+  description: "This is a new t-shirt",
+});
+// insert new item with sub document
+db.products.insertOne({
+  name: "A computer",
+  price: 1229.95,
+  description: "This is a nice laptop.",
+  details: { cpu: "i7", ram: "16GB" },
+});
+
+// show documents in collection
+db.products.find();
